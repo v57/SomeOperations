78a58427dfd3406b071b3fa6e2eece21b4796d5d
Dmitry <d@kozlov.ru>
2020-05-21 14:17:04 +0300

    Added testSyncOperations test

--- Tests/SomeOperationsTests/SomeOperationsTests.swift
+++ Tests/SomeOperationsTests/SomeOperationsTests.swift
@@ -14,8 +14,30 @@ final class SomeOperationsTests: XCTestCase {
     }
     XCTAssertEqual(result, 1)
   }
+  func testSyncOperations() {
+    var result = 0
+    let operations = SomeOperations()
+    operations.add(.run {
+      result = 1
+    })
+    XCTAssertEqual(result, 0)
+    operations.add(.run {
+      result = 2
+    })
+    XCTAssertEqual(result, 0)
+    operations.add(.run {
+      result = 3
+    })
+    XCTAssertEqual(result, 0)
+    operations.run { status, action in
+      XCTAssertEqual(status, .done)
+      XCTAssertEqual(action, .next)
+    }
+    XCTAssertEqual(result, 3)
+  }
   
   static var allTests = [
     ("testSingleSyncOperation", testSingleSyncOperation),
+    ("testSyncOperations", testSyncOperations),
   ]
 }
