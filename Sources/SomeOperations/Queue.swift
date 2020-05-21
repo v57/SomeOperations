0eca6de63a2f3141ed4b6f948cabaff9195a5132
Dmitry <d@kozlov.ru>
2020-05-21 16:48:16 +0300

    Added better queue

--- Sources/SomeOperations/Queue.swift
+++ Sources/SomeOperations/Queue.swift
@@ -7,16 +7,47 @@
 
 import Foundation
 
+class Queue: Operation {
+  var index: Int = 0
+  var operations = [Operation]()
+  var removeCompletedOperations = false
+  var current: Operation? {
     index < operations.count ? operations[index] : nil
   }
+  override func run() {
+    resume()
   }
   func resume() {
-    }
-      done(status: status, action: action)
+    if let current = current {
+      current.queue = self
+      current.run()
+    } else {
+      done()
     }
   }
-  func done(status: SomeOperation.Status, action: SomeOperation.Action) {
-    completion(self, status, action)
+  func reset() {
+    index = 0
+  }
+  func done() {
+    queue?.next()
+  }
+  func insert(_ operations: Operation..., at index: Int, updateIndex: Bool = false) {
+    if updateIndex && self.index >= index {
+      self.index += operations.count
+    }
+    insert(operations, at: index)
+  }
+  func insert(_ operations: [Operation], at index: Int, updateIndex: Bool = false) {
+    self.operations.insert(contentsOf: operations, at: index)
+  }
+  func retry() {
+    resume()
+  }
+  func next() {
+    index += 1
+    resume()
+  }
+}
   }
   func hash(into hasher: inout Hasher) {
     ObjectIdentifier(self).hash(into: &hasher)
