317115a26f480e67ce82a2867feecec08ed0c900
Dmitry <d@kozlov.ru>
2020-05-21 17:41:01 +0300

    Added CustomOperationTests

---
+++ Tests/SomeOperationsTests/CustomOperationsTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import SomeOperations
+
+final class CustomOperationsTests: XCTestCase {
+  func testConnectOperation() {
+    
+  }
+  
+  static var allTests = [
+    ("testConnectOperation", testConnectOperation),
+  ]
+}
+
+class ConnectOperation: Operation {
+  
+}
--- Tests/SomeOperationsTests/XCTestManifests.swift
+++ Tests/SomeOperationsTests/XCTestManifests.swift
@@ -4,6 +4,7 @@ import XCTest
 public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(QueueTests.allTests),
+        testCase(CustomOperationsTests.allTests),
     ]
 }
 #endif
