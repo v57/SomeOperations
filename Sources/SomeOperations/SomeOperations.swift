f34c27259c4badd30d9a3db383d96a1a6ca4dcf6
Dmitry <d@kozlov.ru>
2020-05-21 13:37:24 +0300

    SomeOperations now uses Queue

--- Sources/SomeOperations/SomeOperations.swift
+++ Sources/SomeOperations/SomeOperations.swift
@@ -6,8 +6,15 @@
 //
 
 class SomeOperations: SomeOperation {
-  var operations: [SomeOperation]
+  var operations: [SomeOperation] = []
   override func run(completion: @escaping (Status, Action) -> ()) {
-    
+    state = .running
+    let queue = Queue(operations: operations) { [unowned self] queue, status, action in
+      self.state = .completed
+      self.queues.remove(queue)
+      completion(status, action)
+    }
+    queues.insert(queue)
+    queue.resume()
   }
 }
