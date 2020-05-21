3a7a5e427c150a07c1f7ad5e3a05bddfbd017da6
Dmitry <d@kozlov.ru>
2020-05-21 13:25:43 +0300

    Added SomeOperations

---
+++ Sources/SomeOperations/SomeOperations.swift
@@ -0,0 +1,13 @@
+//
+//  SomeOperations.swift
+//
+//
+//  Created by Dmitry Kozlov on 5/21/20.
+//
+
+class SomeOperations: SomeOperation {
+  var operations: [SomeOperation]
+  override func run(completion: @escaping (Status, Action) -> ()) {
+    
+  }
+}
