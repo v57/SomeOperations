e8c1595eaebe5f9768c4136f1ee060d3a359acb7
Dmitry <d@kozlov.ru>
2020-05-21 18:05:07 +0300

    Test: Added connection

--- Tests/SomeOperationsTests/CustomOperationsTests.swift
+++ Tests/SomeOperationsTests/CustomOperationsTests.swift
@@ -13,4 +13,47 @@ final class CustomOperationsTests: XCTestCase {
 
 class ConnectOperation: Operation {
   
+enum ConnectionError: Error {
+  case lostConnection, noData
+}
+class Connection {
+  var isConnected = false
+  var lastSent: String?
+  func connect(_ completion: @escaping (Result<Void, ConnectionError>)->()) {
+    "ConnectQueue".queue.asyncAfter(deadline: .now() + 0.1) {
+      completion(.success(()))
+    }
+  }
+  func connectFailed(_ completion: @escaping (Result<Void, ConnectionError>)->()) {
+    "ConnectQueue".queue.asyncAfter(deadline: .now() + 0.1) {
+      completion(.failure(.lostConnection))
+    }
+  }
+  func send(_ some: String, completion: @escaping (Result<Void, ConnectionError>)->()) {
+    "ConnectQueue".queue.asyncAfter(deadline: .now() + 0.1) {
+      self.lastSent = some
+      completion(.success(()))
+    }
+  }
+  func sendFailed(_ some: String, completion: @escaping (Result<Void, ConnectionError>)->()) {
+    "ConnectQueue".queue.asyncAfter(deadline: .now() + 0.1) {
+      completion(.failure(.lostConnection))
+    }
+  }
+  func read(_ completion: @escaping (Result<String, ConnectionError>)->()) {
+    "ConnectQueue".queue.asyncAfter(deadline: .now() + 0.1) {
+      if let lastSent = self.lastSent {
+        self.lastSent = nil
+        completion(.success(lastSent))
+      } else {
+        completion(.failure(.noData))
+      }
+    }
+  }
+  func readFailed(_ completion: @escaping (Result<String, ConnectionError>)->()) {
+    "ConnectQueue".queue.asyncAfter(deadline: .now() + 0.1) {
+      completion(.failure(.lostConnection))
+    }
+  }
+}
 }
