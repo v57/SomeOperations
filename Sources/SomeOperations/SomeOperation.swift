85c5b83547c10894b76a7f06c783187d36c546c0
Dmitry <d@kozlov.ru>
2020-05-21 13:25:51 +0300

    Added SomeOperation.Queue

--- Sources/SomeOperations/SomeOperation.swift
+++ Sources/SomeOperations/SomeOperation.swift
@@ -21,4 +21,40 @@ class SomeOperation {
     state = .completed
     completion(.done, .next)
   }
+  class Queue {
+    var index = 0
+    let operations: [SomeOperation]
+    let completion: (Status, Action) -> ()
+    init(index: Int, completion: @escaping (Status, Action) -> ()) {
+      self.operations = operations
+      self.completion = completion
+    }
+    func start() -> Self {
+      next()
+      return self
+    }
+    func next() {
+      guard index < operations.count else {
+        done()
+        return
+      }
+      operations[index].run(completion: process)
+    }
+    func process(status: Status, action: Action) {
+      switch status {
+      case .done:
+        switch action {
+        case .next:
+          index += 1
+          next()
+        }
+        next()
+      case .failed(let error):
+        done(status: status, action: action)
+      }
+    }
+    func done(status: Status, action: Action) {
+      completion(status, action)
+    }
+  }
 }
