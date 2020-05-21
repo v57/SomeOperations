654712f231fd6a9b8715b00d2b9d09217b9092e3
Dmitry <d@kozlov.ru>
2020-05-21 13:41:47 +0300

    Added Async operation

--- Sources/SomeOperations/SampleOperations.swift
+++ Sources/SomeOperations/SampleOperations.swift
@@ -8,4 +8,18 @@
 import Foundation
 
 extension SomeOperation {
+  class Async: SomeOperation {
+    let queue: DispatchQueue
+    let action: ()->(SomeOperation.Status, SomeOperation.Action)
+    init(queue: DispatchQueue, run: @escaping ()->(SomeOperation.Status, SomeOperation.Action)) {
+      self.queue = queue
+      self.action = run
+    }
+    override func run(completion: @escaping (SomeOperation.Status, SomeOperation.Action) -> ()) {
+      queue.async {
+        let (status, action) = self.action()
+        completion(status, action)
+      }
+    }
+  }
 }
