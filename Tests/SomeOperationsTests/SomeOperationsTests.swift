0c7a849594da1d69f0ac1346cb39991d88e868ab
Dmitry <d@kozlov.ru>
2020-05-21 14:15:03 +0300

    Updated tests spacing

--- Tests/SomeOperationsTests/SomeOperationsTests.swift
+++ Tests/SomeOperationsTests/SomeOperationsTests.swift
@@ -2,20 +2,20 @@ import XCTest
 @testable import SomeOperations
 
 final class SomeOperationsTests: XCTestCase {
-    func testSingleSyncOperation() {
-      var result = 0
-      let operation = SomeOperation.run {
-        result = 1
-      }
-      XCTAssertEqual(result, 0)
-      operation.run { status, action in
-        XCTAssertEqual(status, .done)
-        XCTAssertEqual(action, .next)
-      }
-      XCTAssertEqual(result, 1)
+  func testSingleSyncOperation() {
+    var result = 0
+    let operation = SomeOperation.run {
+      result = 1
     }
-
-    static var allTests = [
-        ("testSingleSyncOperation", testSingleSyncOperation),
-    ]
+    XCTAssertEqual(result, 0)
+    operation.run { status, action in
+      XCTAssertEqual(status, .done)
+      XCTAssertEqual(action, .next)
+    }
+    XCTAssertEqual(result, 1)
+  }
+  
+  static var allTests = [
+    ("testSingleSyncOperation", testSingleSyncOperation),
+  ]
 }
