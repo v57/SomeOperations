822abe745867b0c550885b4ae726b80bf0e4cffa
Dmitry <d@kozlov.ru>
2020-05-21 13:35:25 +0300

    Removed unused test

--- Tests/SomeOperationsTests/SomeOperationsTests.swift
+++ Tests/SomeOperationsTests/SomeOperationsTests.swift
@@ -6,7 +6,6 @@ final class SomeOperationsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SomeOperations().text, "Hello, World!")
     }
 
     static var allTests = [
